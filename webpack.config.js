ca555e967911e04fee0d70f420dd029e08d9d428
Diego Gadola <dgadola@gmail.com>
2016-04-11 23:40:53 -0300

    Start using a linter

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,11 @@ module.exports = {
       filename: "[name].js"
   },
   module: {
+    preLoaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: "eslint-loader"
+    }],
     loaders: [{
       test: /\.js$/,
       exclude: /node_modules/,
